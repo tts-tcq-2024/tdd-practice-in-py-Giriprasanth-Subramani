423ceb79f166ef13dc353270537c3e60589b28ba
Giriprasanth-Subramani <167963812+Giriprasanth-Subramani@users.noreply.github.com>
2024-07-08 10:54:28 +0530

    Refactored the code

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,10 +1,12 @@
 def isEmptyString(input):
   if (input == "" or input[0] == '\0'):
     return True
+  return False
     
 def add(input):
   if(isEmptyString(input) == True):
     return 0
+  return -1
   
     
   
