2227df36689b300be85689bbce03a1991e1c84d2
Giriprasanth-Subramani <167963812+Giriprasanth-Subramani@users.noreply.github.com>
2024-07-08 10:51:40 +0530

    Refactored the Add function

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,5 +1,10 @@
-def add(input):
+def isEmptyString(input):
   if (input == "" or input[0] == '\0'):
+    return True
+    
+def add(input):
+  if(isEmptyString(input) == True):
     return 0
+  
     
   
