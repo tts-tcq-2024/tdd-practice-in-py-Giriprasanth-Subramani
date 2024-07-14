012a0fdd49bd83190efec59aaab16bad3eca1f87
Giriprasanth-Subramani <167963812+Giriprasanth-Subramani@users.noreply.github.com>
2024-07-14 23:52:00 +0530

    new functions added

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,12 +1,31 @@
-def isEmptyString(input):
-  if (input == "" or input[0] == '\0'):
-    return True
-  return False
+import re
+
+def check_negatives(numbers):
+     for value in numbers:
+         try:
+             if value.startswith('-'):                 
+                 raise ValueError(f"Negatives not allowed: {value}")                 
+         except ValueError as e:
+            print(e)
+          
+        
+def generate_number_list(input):    
+        numbers = re.findall(r'-\d+', input)
+        if not numbers:
+            numbers = re.findall(r'\d+', input)
+        else:
+            numbers = check_negatives(numbers)
+            
+        return numbers
     
 def add(input):
-  if(isEmptyString(input) == True):
-    return 0
-  return -1
+    
+    numbers = generate_number_list(input)   
+    number_list = []
+    for number in numbers:
+        if int(number) < 1000:
+            number_list.append(int(number))
+    return sum(number_list)
   
     
   
